cbafbffc31a7953308ee1b67d1acb14a1ef60e69
WillChew <williamchew95@gmail.com>
2018-07-18 13:31:29 -0400

    before adding images in requestmanager

---
+++ Quote Pro/Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  Quote Pro
+//
+//  Created by Will Chew on 2018-07-18.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class Image {
+    var link: String
+    
+    init(link: String) {
+        self.link = link
+    }
+}
