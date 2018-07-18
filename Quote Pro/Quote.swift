ca7aef83c6a3c4bbf7ceaba02bcf4bee7eadeb9d
WillChew <williamchew95@gmail.com>
2018-07-18 11:54:52 -0400

    added quote class

---
+++ Quote Pro/Quote.swift
@@ -0,0 +1,19 @@
+//
+//  Quote.swift
+//  Quote Pro
+//
+//  Created by Will Chew on 2018-07-18.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class Quote {
+    var quote: String
+    var author: String?
+    
+    init(quote: String, author: String?) {
+        self.quote = quote
+        self.author = author ?? "No author"
+    }
+}
